93984cb571114c95a2d4f9ff1605a11840fe0132
Dany Raimundo <Danyiejota@gmail.com>
2023-07-30 14:09:28 +0100

    Lesson #86

---
+++ Bulky.DataAccess/Migrations/20230730130314_addForeignKeyBetweenCategoryandProductRelation.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bulky.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addForeignKeyBetweenCategoryandProductRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryID",
+                table: "Product",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Product",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CategoryID",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Product",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CategoryID",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Product",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CategoryID",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Product",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CategoryID",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Product",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CategoryID",
+                value: 3);
+
+            migrationBuilder.UpdateData(
+                table: "Product",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "CategoryID",
+                value: 2);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_CategoryID",
+                table: "Product",
+                column: "CategoryID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_Categories_CategoryID",
+                table: "Product",
+                column: "CategoryID",
+                principalTable: "Categories",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_Categories_CategoryID",
+                table: "Product");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Product_CategoryID",
+                table: "Product");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryID",
+                table: "Product");
+        }
+    }
+}
--- BulkyModels/Product.cs
+++ BulkyModels/Product.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -37,5 +38,9 @@ namespace Bulky.Models
 		[Display(Name = "Price for 100+")]
 		[Range(1, 1000)]
 		public double Price100 { get; set; }
-	}
+
+		public int CategoryID { get; set; }
+		[ForeignKey("CategoryID")]
+        public Category Category { get; set; }
+    }
 }
